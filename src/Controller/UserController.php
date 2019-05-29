0d2c4e31fa871d34fb4ecec322214903630fb33a
Christine BRASSART <chrisbras89@gmail.com>
2019-05-29 13:18:43 +0200

    created a UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function show(User $user)
+    {
+        return $this->render('user/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
