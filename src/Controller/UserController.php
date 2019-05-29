fdb6b61f7e0fac428ee0a750cea195bdba294b73
LDorine <dodosav.fr@gmail.com>
2019-05-29 15:41:02 +0200

    Adding controller for form user sign up

---
+++ src/Controller/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserSignUpType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/new", name="user_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request) : Response
+    {
+        $userSignUp = new User();
+        $form = $this->createForm(UserSignUpType::class,$userSignUp);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $userSignUp = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($userSignUp);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_show');
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $userSignUp,
+            'form' => $form->createView()
+        ]);
+
+    }
+}
