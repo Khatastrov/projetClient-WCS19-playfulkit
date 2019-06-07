179d09f4964956a016dbe23dc8adecf10b27e797
Alexandre Pasquier <alexandre.pasquier91@gmail.com>
2019-06-07 16:34:39 +0200

    adding service directory

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use App\Entity\Tutorial;
 use App\Form\TutorialType;
 use App\Repository\TutorialRepository;
+use App\Service\FileUploader;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
 
 class TutorialController extends AbstractController
 {
@@ -31,11 +30,17 @@ class TutorialController extends AbstractController
     /**
      * @Route("/tutorial/new", name="tutorial_create")
      * @Route("/tutorial/{id}/edit", name="tutorial_edit")
+     * @param Tutorial|null $tuto
+     * @param Request $request
+     * @param ObjectManager $manager
+     * @param FileUploader $fileUploader
+     * @return Response
+     * @throws \Exception
      */
-    public function form(Tutorial $tuto = null ,Request $request, ObjectManager $manager)
+    public function form(Tutorial $tuto = null, Request $request, ObjectManager $manager, FileUploader $fileUploader)
+    : Response
     {
-        if(!$tuto)
-        {
+        if (!$tuto) {
             $tuto = new Tutorial();
         }
 
@@ -43,14 +48,16 @@ class TutorialController extends AbstractController
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
-        {
-
-
-            if(!$tuto->getId())
-            {
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$tuto->getId()) {
                 $tuto->setDateCreation(new \DateTime());
             }
+
+            $file = new FileUploader($tuto->getIllustration());
+            $fileName= $fileUploader->upload($file);
+
+
+            $tuto->setIllustration($fileName);
 
             $manager->persist($tuto);
             $manager->flush();
