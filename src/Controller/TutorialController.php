c6095534c6149f02ba31fc166a5bca113bc88750
elie <elie.verholle@gmail.com>
2019-06-12 15:49:12 +0200

    modif pour le form

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -7,9 +7,10 @@ use App\Form\TutorialType;
 use App\Repository\TutorialRepository;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Validator\Constraints\Choice;
 
 
 class TutorialController extends AbstractController
@@ -31,36 +32,34 @@ class TutorialController extends AbstractController
      * @Route("/tutorial/new", name="tutorial_create")
      * @Route("/tutorial/{id}/edit", name="tutorial_edit")
      */
-    public function form(Tutorial $tuto = null ,Request $request, ObjectManager $manager)
+    public function form(Tutorial $tuto = null, Request $request)
     {
-        if(!$tuto)
+        if (!$tuto)
         {
             $tuto = new Tutorial();
         }
+
 
         $form = $this->createForm(TutorialType::class, $tuto);
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid())
+        if ($form->isSubmitted() && $form->isValid())
         {
-            if(!$tuto->getId())
-            {
-                $tuto->setDateCreation(new \DateTime());
-            }
 
+
+                $tuto->setDateCreation(new \DateTime());
+
+
+            $manager = $this->getDoctrine()->getManager();
             $manager->persist($tuto);
             $manager->flush();
-
-            return $this->redirectToRoute('tutorial_show', [
-                'id' => $tuto->getId()
-            ]);
         }
 
         return $this->render('tutorial/create.html.twig', [
-            'formTutorial' =>$form->createView(),
-            'editMode' => $tuto->getId() !== null,
+            'formTutorial' => $form->createView()
         ]);
+
     }
 
 
