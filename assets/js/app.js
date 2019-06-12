90ff3ba6df2c281a703940991c77126e335f73f5
kej1n <45516457+kej1n@users.noreply.github.com>
2019-06-12 10:39:03 +0200

    creation of collapse, modify base for testing purpose.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,7 @@ require('../scss/app.scss');
 require('bootstrap');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
- const $ = require('jquery');
+require('jquery-ui');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+console.log('Ok App !');
