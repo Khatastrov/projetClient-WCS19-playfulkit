dfb213ac55aa5aa51cc92a784fd9754e0c0df9b7
Christine BRASSART <chrisbras89@gmail.com>
2019-06-15 17:36:54 +0200

    modified log-in process

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
@@ -47,9 +48,18 @@ class UserController extends AbstractController
       /**
        * @Route("/connexion", name="user_login")
        */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('user/login.html.twig');
+        // get the login error if any
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        // usefull if "remember me" option activated
+        //$lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            //'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 
       /**
