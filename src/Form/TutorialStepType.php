382644814db7a7fa6099e9c490b22d1af3c1af02
Christine BRASSART <chrisbras89@gmail.com>
2019-06-17 13:23:09 +0200

    added image loading in tutorial step part. (was forced to add 'date_creation' to entity tutorialStep.php)

--- src/Form/TutorialStepType.php
+++ src/Form/TutorialStepType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\TutorialStep;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,9 @@ class TutorialStepType extends AbstractType
         $builder
             ->add('title')
             ->add('content')
-            ->add('image')
+            ->add('imageFile', FileType::class, [
+                'required'=> false,
+            ])
         ;
     }
 
