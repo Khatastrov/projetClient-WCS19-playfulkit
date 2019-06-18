b8b7e77b161f513a2ea09235826eab7bce1a67a6
kej1n <45516457+kej1n@users.noreply.github.com>
2019-06-18 19:23:40 +0200

    Style tuto show
    Fixing button issues, adding some responsive and color on elements, gona test with a new tutorial creration.

--- assets/js/tuto.js
+++ assets/js/tuto.js
@@ -5,13 +5,13 @@ console.log('Ok tuto !');
 $('.customCollapse button').on('click', function(){
     if ($(this).hasClass('isOpen')){
         $('.customCollapse article').stop();
-        $(this).siblings('article').slideUp(400);
+        $(this).parent('.row').siblings('article').slideUp(400);
         $(this).removeClass('isOpen').text('+');
     }
 
     else{
         $('.customCollapse article').stop();
-        $(this).siblings('article').slideDown(400);
+        $(this).parent('.row').siblings('article').slideDown(400);
         $(this).addClass('isOpen').text('-');
     }
 });
