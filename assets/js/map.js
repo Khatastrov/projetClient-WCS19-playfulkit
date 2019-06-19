4d981b4ff6a6ed90ef73510b2829a56d33ad5879
LDorine <dodosav.fr@gmail.com>
2019-06-19 15:59:44 +0200

    Adding code of first js test

---
+++ assets/js/map.js
@@ -0,0 +1,10 @@
+require('../scss/map.scss');
+
+let map;
+function initMap()
+{
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
