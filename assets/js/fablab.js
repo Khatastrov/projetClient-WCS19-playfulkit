4ea2aa3343d743cf4e7625479aaee43f7081b676
LDorine <dodosav.fr@gmail.com>
2019-06-25 11:58:27 +0200

    Adding js function for show fablab name

--- assets/js/fablab.js
+++ assets/js/fablab.js
@@ -1,19 +1,14 @@
-let fablabElt = document.getElementById("articles");
-ajaxGet("http://www.makery.info/api/labs/", function (reponse) {
-    // Transforme la réponse en un tableau d'articles
-    let features = JSON.parse(reponse);
-    features.forEach(function (feature) {
-        let properties = JSON.parse(reponseProperties);
-            properties.forEach(function (propertie) {
-                if (propertie.type_lab == "fablab") {
-                    let nameElt = document.createElement('h3');
-                    nameElt.name = propertie.name;
-                    fablabElt.appendChild(nameElt);
-                } else {
-                    let alertElt = document.createElement('p');
-                    alertElt.alert = "Not found !";
-                }
-            })
+let fablabElt = document.getElementById("fablab");
+$.getJSON("http://www.makery.info/api/labs/", function (data) {
+    //console.log(data.features);
+    const fablabs = data.features.filter((feature)=>{
+        return feature.properties.type_lab === 'fablab';
+    });
+    //console.log(fablabs);
+    fablabs.map((fablab)=>{
+        let nameElt = document.createElement('h3');
+        nameElt.innerText = fablab.properties.name;
+        fablabElt.appendChild(nameElt);
     });
 });
 
