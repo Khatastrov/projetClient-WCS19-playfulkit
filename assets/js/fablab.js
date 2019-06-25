bb004288df91890bc46408c609ed796700d6cee9
LDorine <dodosav.fr@gmail.com>
2019-06-25 11:58:54 +0200

    Adding js function for show fablab name

--- assets/js/fablab.js
+++ assets/js/fablab.js
@@ -1,10 +1,8 @@
 let fablabElt = document.getElementById("fablab");
 $.getJSON("http://www.makery.info/api/labs/", function (data) {
-    //console.log(data.features);
     const fablabs = data.features.filter((feature)=>{
         return feature.properties.type_lab === 'fablab';
     });
-    //console.log(fablabs);
     fablabs.map((fablab)=>{
         let nameElt = document.createElement('h3');
         nameElt.innerText = fablab.properties.name;
