916f8b817fd48fb5a16591bf255d445814c41d1e
florian Logez <flo.log09@gmail.com>
2019-06-26 11:02:08 +0200

    addding lesson part in sonata

---
+++ src/Admin/LessonAdmin.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Admin;
+
+use App\Entity\Category;
+use App\Entity\Tool;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class LessonAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('title', TextType::class);
+        $formMapper->add('category', ModelType::class, [
+            'class' => Category::class,
+            'property' => 'name',
+        ]);
+        $formMapper->add('tool', ModelType::class, [
+            'class' => Tool::class,
+            'property' => 'name',
+            'multiple' => true
+        ]);
+        $formMapper->add('content', TextType::class);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('title');
+    }
+}
