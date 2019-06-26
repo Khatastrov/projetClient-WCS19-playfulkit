0a2ffec461aba2f522d754cd021798a620da2477
LDorine <dodosav.fr@gmail.com>
2019-06-26 19:38:29 +0200

    Change form to call tooltype

--- src/Form/TutorialToolType.php
+++ src/Form/TutorialToolType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\TutorialTool;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,16 @@ class TutorialToolType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('tool')
-            ->add('quantity');
+            ->add('quantity')
+            ->add('tool', CollectionType::class, [
+                'label' => false,
+                'entry_type' => ToolType::class,
+                'required' => false,
+                'entry_options' => ['label' => false],
+                'by_reference' => false,
+                'allow_add' => true,
+                'allow_delete' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
