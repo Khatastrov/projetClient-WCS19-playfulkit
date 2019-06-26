ad937afbd8466aa14365f668654844c4af2e3422
LDorine <dodosav.fr@gmail.com>
2019-06-26 19:37:58 +0200

    Adding form of tool entity, progress working

---
+++ src/Form/ToolType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tool;
+use App\Repository\ToolRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ToolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', EntityType::class, [
+                'class' => Tool::class,
+                //'query_builder' => $tool->getNameByCategory(),
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tool::class,
+        ]);
+    }
+}
