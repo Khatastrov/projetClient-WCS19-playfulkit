11355c9865b9892c03a150532a7a5408e966d662
elie <elie.verholle@gmail.com>
2019-06-27 12:38:30 +0200

    form, template, entity for receiving and returning an email

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/contact")
+ */
+class ContactController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="contact", methods={"GET","POST"})
+     */
+    public function new(Request $request, \Swift_Mailer $mailer): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            $message = (new \Swift_Message('J\'ai bien reçu votre email'))
+                ->setFrom('adresse_mail@gmail.com')
+                ->setTo('adresse_mail@gmail.com')
+                ->setBody ($this->renderView('contact/renvoi.html.twig'), 'text/html');
+            $mailer->send($message);
+        }
+
+        return $this->render('contact/new.html.twig', [
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ]);
+    }
+
+}
