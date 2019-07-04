1c006fad6c06be8340f0964d388e7890eb8b422c
kej1n <45516457+kej1n@users.noreply.github.com>
2019-07-04 17:29:00 +0200

    fix on slick btn

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -4,10 +4,9 @@ require('../scss/homePage.scss');
 $('.one-time').slick({
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     fade: true,
     cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 3000,
-    
 });
