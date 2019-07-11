1d7b3ad6600dcfd4add7b6dccbd08c4a9c257676
florian Logez <flo.log09@gmail.com>
2019-07-11 16:03:30 +0200

    finish contest part

--- src/Controller/ContestController.php
+++ src/Controller/ContestController.php
@@ -3,16 +3,37 @@
 
 namespace App\Controller;
 
+use App\Entity\InterestedPeople;
+use App\Form\InterestedPeopleType;
+use App\Repository\InterestedPeopleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContestController extends AbstractController
 {
     /**
-     * @Route("/contest", name="contest
+     * @Route("/contest", name="contest")
      */
-    public function index()
+    public function index(Request $request): Response
     {
-        return $this->render('contest/index');
+        $client = new InterestedPeople();
+
+        $form = $this->createForm(InterestedPeopleType::class, $client);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($client);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Ton adresse est enregistrée ! Bien joué l\'ami !');
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('contest/index.html.twig', [
+            'form' => $form->createView(),
+            ]);
     }
 }
