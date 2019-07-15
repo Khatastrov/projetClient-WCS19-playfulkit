d31108622f0e17387f54404d57b4c0ad1f7db959
florian Logez <flo.log09@gmail.com>
2019-07-15 10:29:29 +0200

    adding image in tuto index

--- src/Admin/ToolAdmin.php
+++ src/Admin/ToolAdmin.php
@@ -7,6 +7,7 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 final class ToolAdmin extends AbstractAdmin
@@ -14,6 +15,7 @@ final class ToolAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper->add('name', TextType::class);
+        $formMapper->add('quantity', IntegerType::class);
         $formMapper->add('category', ChoiceType::class, [
             'choices' => [
             'Handtool' => 'handtool',
